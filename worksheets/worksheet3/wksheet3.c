374a9370656514d338b2265d9f6e5a7b6e9b9a2a
RobertMercieca <robert.mercieca.a101124@mcast.edu.mt>
2020-11-06 16:20:27 +0100

    worksheet 3

---
+++ worksheets/worksheet3/wksheet3.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// int main(int argc, char *argv[])
+// {
+//   printf("Programs name %s\n", argv[0]);
+
+//   if (argc == 1)
+//   {
+//     printf("No arguments supplied\n");
+//   }
+//   else
+//   {
+//     for (int i = 1; i < argc; i++)
+//     {
+//       printf("argc[%d] = %s\n", i, argv[i]);
+//     }
+//   }
+// }
+
+int main(int argc, char *argv[])
+{
+
+  // No 1
+
+  // int num1, num2;
+
+  // if (argc < 3)
+  // {
+  //   printf("Too little arguments supplied.\n");
+  // }
+  // else if (argc > 3)
+  // {
+  //   printf("Too many arguments supplied.\n");
+  // }
+  // else
+  // {
+  //   num1 = atoi(argv[1]);
+  //   num2 = atoi(argv[2]);
+
+  //   if (num1 < num2)
+  //   {
+  //     printf("%d is smaller than %d\n", num1, num2);
+  //   }
+  //   else if (num1 > num2)
+  //   {
+  //     printf("%d is smaller than %d\n", num2, num1);
+  //   }
+  //   else
+  //   {
+  //     printf("They are equal\n");
+  //   }
+  // }
+
+  //No 2
+
+  // if (argc < 2)
+  // {
+  //   printf("Too little arguments\n");
+  //   return -1;
+  // }
+
+  // double total;
+  // for (int i = 1; i < argc; i++)
+  // {
+  //   total += atof(argv[i]);
+  // }
+  // printf("total is: %.2f\n", total);
+  // printf("average is: %.2f\n", total / (argc - 1));
+
+  // return 0;
+}
